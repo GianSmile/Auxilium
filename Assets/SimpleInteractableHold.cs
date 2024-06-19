3fe885c7544f8f35043be150bd5d20dd9cbe6132
Maximiliano Fernandez <113542968+Otagis@users.noreply.github.com>
2024-06-19 10:30:35 -0700

    .

--- Assets/SimpleInteractableHold.cs
+++ Assets/SimpleInteractableHold.cs
@@ -4,7 +4,35 @@ using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class SimpleInteractableHold : XRSimpleInteractable
+public class SimpleInteractableHold : XRGrabInteractableTwoAttach
 {
     //Hold on call script
+    public GameObject ControllerL;
+    public GameObject ControllerR;
+
+    public Transform leftAttachTransform;
+    public Transform rightAttachTransform;
+
+    private void Start()
+    {
+        ControllerL = GameObject.Find("Left Hand");
+        ControllerR = GameObject.Find("Right Hand");
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.CompareTag("Right Hand"))
+        {
+            attachTransform = rightAttachTransform;
+            Debug.Log("R");
+        }
+
+        else if (args.interactorObject.transform.CompareTag("Left Hand"))
+        {
+            attachTransform = leftAttachTransform;
+            //ControllerL.GetComponent<XRController>().
+            Debug.Log("L");
+        }
+        base.OnSelectEntered(args);
+    }
 }
