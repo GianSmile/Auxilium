ae28e94c28431186fb5e76cfbed610d0474dc2ba
GianSmile <86799576+GianSmile@users.noreply.github.com>
2024-06-26 13:30:15 -0300

    bugfix pase de niveles

--- Assets/Scripts/Gameplay/PassLevel.cs
+++ Assets/Scripts/Gameplay/PassLevel.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class PassLevel : MonoBehaviour
 {
     public bool isVR;
-    public GameDataController gdc;
+    private GameDataController gdc;
+    public int passLevel = 0;
 
     private void Start() {
         gdc = GameObject.FindGameObjectWithTag("Controller").GetComponent<GameDataController>();
@@ -14,34 +15,12 @@ public class PassLevel : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.J)){
-            if(SceneManager.GetActiveScene().name == "RCP"){
-                if(!isVR)SceneManager.LoadScene("Main Menu");
-                if(isVR)SceneManager.LoadScene("Main Menu VR");
+            if (!isVR) SceneManager.LoadScene("Main Menu");
+            if (isVR) SceneManager.LoadScene("Main Menu VR");
+
+            if(passLevel != 0)
+            {
                 gdc.PassLevel(1);
-            }
-            if(SceneManager.GetActiveScene().name == "Problemas respiratorios"){
-                if(!isVR)SceneManager.LoadScene("Main Menu");
-                if(isVR)SceneManager.LoadScene("Main Menu VR");
-                gdc.PassLevel(2);
-            }
-            if(SceneManager.GetActiveScene().name == "Quemaduras"){
-                if(!isVR)SceneManager.LoadScene("Main Menu");
-                if(isVR)SceneManager.LoadScene("Main Menu VR");
-                gdc.PassLevel(3);
-            }
-            if(SceneManager.GetActiveScene().name == "Intoxicaciones"){
-                if(!isVR)SceneManager.LoadScene("Main Menu");
-                if(isVR)SceneManager.LoadScene("Main Menu VR");
-                gdc.PassLevel(4);
-            }
-            if(SceneManager.GetActiveScene().name == "Cortes y heridas"){
-                if(!isVR)SceneManager.LoadScene("Main Menu");
-                if(isVR)SceneManager.LoadScene("Main Menu VR");
-                gdc.PassLevel(5);
-            }
-            if(SceneManager.GetActiveScene().name == "Reacciones alérgicas"){
-                if(!isVR)SceneManager.LoadScene("Main Menu");
-                if(isVR)SceneManager.LoadScene("Main Menu VR");
             }
         }
     }
